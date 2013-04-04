98eaf9ad96441e1fb6d7499069afb64d60ab62a4
Jim Yeh <lemonlatte@gmail.com>
2013-04-04 10:45:40 +0800

    Add configuration of pdb++
    Change-Id: I226a4c843e392c9f6446c1c86333468ef453a4b2

---
+++ .pdbrc.py
@@ -0,0 +1,18 @@
+import pdb
+
+
+class Config(pdb.DefaultConfig):
+    prompt = '(Pdb++) '
+    highlight = True
+    encoding = 'utf-8'
+    sticky_by_default = False
+    # line_number_color = Color.turquoise
+    # filename_color = Color.yellow
+    current_line_color = 44
+    use_pygments = True
+    bg = 'dark'
+    colorscheme = None
+    editor = '${EDITOR:-vi}'
+    truncate_long_lines = True
+    exec_if_unfocused = None
+    disable_pytest_capturing = True
