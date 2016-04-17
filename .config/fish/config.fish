39c4bebba8e53bbe0ebcbab33c69126f3c7de45f
Jim Yeh <lemonlatte@gmail.com>
2016-04-17 11:11:38 +0800

    Use powerline shell instead of customized prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,14 +3,14 @@
 egrep "^export " ~/.bash_profile | while read e
 	set var (echo $e | sed -E "s/^export ([A-Z_]+)=(.*)\$/\1/")
 	set value (echo $e | sed -E "s/^export ([A-Z_]+)=(.*)\$/\2/")
-	
+
 	# remove surrounding quotes if existing
 	set value (echo $value | sed -E "s/^\"(.*)\"\$/\1/")
 
 	if test $var = "PATH"
 		# replace ":" by spaces. this is how PATH looks for Fish
 		set value (echo $value | sed -E "s/:/ /g")
-	
+
 		# use eval because we need to expand the value
 		eval set -xg $var $value
 
@@ -25,9 +25,15 @@ egrep "^export " ~/.bash_profile | while read e
 end
 
 set -xg EDITOR 'subl -w'
+set -xg VIRTUALFISH_HOME "$HOME/.venvs"
+set -xg VIRTUALFISH_COMPAT_ALIASES
+# Python VirtualENV for fish
+source $HOME/.config/fish/virtual.fish
+# NVM for fish
+source ~/.config/fish/nvm-wrapper/nvm.fish
+
 alias grep='grep --color=auto'
 alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias cleanpyc='find . -name \*.pyc -delete'
-alias gitst='git status'
-alias gitsppp='git stash;git pull --rebase;git push;git stash pop'
+
---
+++ .config/fish/functions/fish_right_prompt.fish.bak
@@ -0,0 +1,3 @@
+function fish_right_prompt -d "Right prompt in Fish Shell"
+    echo (date "+%H:%M:%S %m/%d/%Y")
+end
